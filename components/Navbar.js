3f5d4ac2c6eb982de65b0e722d67fbf8308fe098
Bharathmogaveer <bharathmogaveer01@gmail.com>
2022-07-17 20:05:47 +0530

    update clear search icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 import Styles from "../Styles/Navbar.module.css";
 import Search from "./Icons/Search";
+import Close from "./Icons/Close";
 import Bell from "./Icons/Bell";
 import Image from "next/image";
 import Logo from "../assets/Logo.png";
@@ -10,20 +11,27 @@ const Navbar = (props) => {
       <div className={Styles.searchbar_container}>
         <div className={Styles.search_icon}>
           <div>
-            {props.isSearchbarOpen ? <button onClick={props.clearSearchbar}>
-              X
-            </button> : <Search />}
+            {props.isSearchbarOpen ? (
+              <Close  onClick={props.clearSearchbar} height={"22px"} width={"16px"} />
+            ) : (
+              <Search />
+            )}
           </div>
-          <div >
-            <input className={Styles.input_element} type={"text"} placeholder={"Search by account name or website"}  onFocus={props.handleSearchbarFocus}/>
+          <div>
+            <input
+              className={Styles.input_element}
+              type={"text"}
+              placeholder={"Search by account name or website"}
+              onFocus={props.handleSearchbarFocus}
+              onChange={props.handleSearch}
+            />
           </div>
         </div>
         <div className={Styles.Bell_container}>
           <div className={Styles.bell}>
             <Bell />
-
           </div>
-          <div className={Styles.logo}> 
+          <div className={Styles.logo}>
             <Image src={Logo} alt="logo" />
           </div>
         </div>
