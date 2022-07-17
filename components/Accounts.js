f6dc27f212393ec0eccb240137ea0cabb3890555
Bharathmogaveer <bharathmogaveer01@gmail.com>
2022-07-17 23:25:26 +0530

    add log tracking info

--- components/Accounts.js
+++ components/Accounts.js
@@ -10,57 +10,50 @@ const Accounts = (props) => {
     setIsTracking(true);
   };
 
-  const handleStopTracking = () => {
-    setIsTracking(false);
+  const logTracking = () => {
+    console.log(
+      `${props.company} (${props.slug}) tracked at ${new Date().toDateString()}`
+    );
   };
 
   return (
     <div>
-      <div>
-        <div className={Styles.accounts_container}>
-          <div className={Styles.accounts_Subcontainer}>
-            <div className={Styles.accounts_logo}>
-              {props.logo ? (
-                <img
-                  src={props.logo}
-                  alt="logo"
-                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
-                />
-              ) : (
-                <div className={Styles.accounts_company_logo_name}>
-                  {props.company[0]}
-                </div>
-              )}
-            </div>
-            <div>
-              <div className={Styles.company_name}>{props.company}</div>
-              <div className={Styles.company_website}>{props.website}</div>
-            </div>
+      <div className={Styles.accounts_container}>
+        <div className={Styles.accounts_Subcontainer}>
+          <div className={Styles.accounts_logo}>
+            {props.logo ? (
+              <img
+                src={props.logo}
+                alt="logo"
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            ) : (
+              <div className={Styles.accounts_company_logo_name}>
+                {props.company[0]}
+              </div>
+            )}
           </div>
-
-          {isTracking ? (
-            <div>
-              <button
-                onClick={handleTracking}
-                className={Styles.button_container_green}
-              >
-                <div className={Styles.buttton_text_green}>Tracking</div>
-              </button>
-            </div>
-          ) : (
-            <div>
-              <button
-                onClick={handleTracking}
-                className={Styles.button_container_red}
-              >
-                <div className={Styles.buttton_text_red}>Track</div>
-              </button>
-            </div>
-          )}
+          <div>
+            <div className={Styles.company_name}>{props.company}</div>
+            <div className={Styles.company_website}>{props.website}</div>
+          </div>
         </div>
+        {isTracking ? (
+          <button
+            onClick={logTracking}
+            className={Styles.button_container_green}
+          >
+            Tracking
+          </button>
+        ) : (
+          <button
+            onClick={handleTracking}
+            className={Styles.button_container_red}
+          >
+            Track
+          </button>
+        )}
       </div>
-
-      <div></div>
     </div>
   );
 };
